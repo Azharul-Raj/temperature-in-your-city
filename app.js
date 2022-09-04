7c1ea5d92208ec3c3d163163ddd127604f55c2ee
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-04 15:06:48 +0600

    display data function done

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const loadTemperature = async (city) => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`    
         const res = await fetch(url)
         const data = await res.json()
-        console.log(data)
+        displayTemperature(data)
         }
     else {
         const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=0f6d0834c2e6a7d0e6c4c051e30faf66&units=metric`)
         const data = await res.json()
-        console.log(data)
+        displayTemperature(data)
     }
 }
+const displayTemperature = (data) => {
+    const { name, main, weather } = data
+    console.log(main, weather)
+    setInnerText('temperature',main.temp)
+}
+
+// setting innerHTML function
+const setInnerText = (setPlaceId, setData) => {
+    const setPlace = document.getElementById(setPlaceId)
+    setPlace.innerText = setData
+}
+// set
 loadTemperature()
