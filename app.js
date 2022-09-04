5ffbc167d8afb7779dafd47c4e284e840892baba
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-04 16:41:09 +0600

    enter search function added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API_KEY = `0f6d0834c2e6a7d0e6c4c051e30faf66&units=metric`
 const loadTemperature = async (city) => {
+    console.log(city)
     if (city) {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`    
         const res = await fetch(url)
@@ -29,4 +30,24 @@ const setAllData = (id1,id2,id3,data1,data2,data3) => {
     setInnerText(id2, data2)
     setInnerText(id3, data3)
 }
+// add event listener in the search btn
+document.getElementById('search-btn').addEventListener('click', () => {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value
+    loadTemperature(searchText)
+    searchField.value = ``
+})
+// event listener for enter btn
+document.getElementById('search-field').addEventListener('keyup', (event) => {
+    const keyPress = event.key
+    console.log(keyPress)
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value
+    if (keyPress === 'Enter') {
+        loadTemperature(searchText)
+        searchField.value = ``
+    }
+    
+})
+
 loadTemperature()
